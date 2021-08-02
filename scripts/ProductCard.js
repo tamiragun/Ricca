4766238c9f727383575275f4c46144237104a106
Tamira <t.gunzburg@gmail.com>
2021-08-02 16:29:58 +0200

    Convert product card to Web Component for reuseability

--- scripts/ProductCard.js
+++ scripts/ProductCard.js
@@ -1,24 +1,39 @@
 //Reusable HTML element, courtesy of https://dev.to/anuradha9712/create-reusable-web-components-in-html-1llc
-
-const productCardTemplate = document.createElement("template");
-productCardTemplate.innerHTML = `
-
-<div>
-    <link href="css/product-card.css" rel="stylesheet">
-    <div class="product card">
-            <h1> Header - My First Blog on Web Component </h1>
-    </div>
-</div>`;
+//and https://developers.google.com/web/fundamentals/web-components/customelements
 
 class ProductCard extends HTMLElement {
-  constructor() {
-    // Always call super first in constructor
-    super();
-  }
-  //attach all the created elements to the shadow root
   connectedCallback() {
-    const shadowRoot = this.attachShadow({ mode: "closed" });
-    shadowRoot.appendChild(productCardTemplate.content);
+    this.innerHTML = `
+     <section class="products">
+     <div class="product-card">
+     <div class="product-image">
+         <img src=${this.getAttribute("src")} alt=${this.getAttribute("alt")} />
+     </div>
+     <div class="product-description">
+         <div class="product-title-description">
+         <h4 class="product-title">${this.getAttribute("product-title")}</h4>
+         <p class="product-text">
+         ${this.getAttribute("text")}
+         </p>
+         </div>
+         <div class="product-info">
+         <div class="product-prices">
+             <div class="old-price">${this.getAttribute("old-price")}</div>
+             <div class="new-price">${this.getAttribute("new-price")}</div>
+         </div>
+         <div class="product-icons">
+             <a href="#"
+             ><img src="./assets/heart.svg" alt="heart icon"
+             /></a>
+             <a href="#"
+             ><img src="./assets/cart.svg" alt="cart icon"
+             /></a>
+         </div>
+         </div>
+     </div>
+     </div>
+     </section>
+     `;
   }
 }
 
